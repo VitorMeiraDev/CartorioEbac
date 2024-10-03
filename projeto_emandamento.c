f1700b410a96908f2e42b05c3c87ebf507b8fb9d
VitorMeiraDev <vitorpeixotomeira@gmail.com>
2024-10-03 13:35:13 -0300

    Opção de Saida
    Incluso no sistema opção de saida para usuário

--- projeto_emandamento.c
+++ projeto_emandamento.c
@@ -128,7 +128,8 @@ int main()
        printf("Escolha a opção desejada do menu:\n\n"); 
        printf("\t1 - Registrar nomes\n");
        printf("\t2 - Consultar nomes\n");
-       printf("\t3 - Deletar nomes\n"); //fim do menu
+       printf("\t3 - Deletar nomes\n\n");
+	   printf("\4 - Sair do sistema\n\n");  //fim do menu
     
        scanf("%d",&opcao); //armazenamento a escolha do usuário
     
@@ -147,6 +148,11 @@ int main()
                     
                     case 3:
                     deletar();
+                    break;
+                    
+                    case 4:
+                    printf("Obrigado por utilizar o sistema!\n");
+                    return 0;
                     break;
                     
                     default:
